a5cbe6c811977ea5d6e963dab9e98212523d6578
MisterManley <george@themanleystudio.com>
2021-07-11 14:21:28 -0500

    Completed palindrome checker

--- scripts.js
+++ scripts.js
@@ -4,3 +4,31 @@
 
 /* Question 3: letting a function return a value lets javascript, and other languages, influence the behavior of web pages and applications. This can make them dynamic */
 
+
+
+function checkPalindrome(string){
+    let re = /[\W_]/g;
+    //find the length of "string", convert it to lowercase and replace all non-string characters with an empty string[not a space character], search globally (/g). Source: freeCodeCamp.org
+    string = string.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+    const length = string.length;
+
+    // loops through the first half of the string (length/2)
+    for(let i = 0; i < length/2; i++){
+
+        //compares characters from the first and second half of the string for equality first half (length / 2) second half (length - 1 - i)
+        if(string[i] !== string[length - 1 - i]){
+            return "This is not a palindrome";
+        } else {
+            
+        }
+    }
+    return "This is a palindrome";
+       
+     
+}
+
+
+
+let palinCandidate = "Mom";
+const result = checkPalindrome(palinCandidate);
+console.log(result);
