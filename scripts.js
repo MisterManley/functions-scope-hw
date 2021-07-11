7792df31020a09c95c61cd86d4e107e5aafdaf57
MisterManley <george@themanleystudio.com>
2021-07-11 12:53:35 -0500

    Added index.html scripts.js style.css answered Q1-Q3

---
+++ scripts.js
@@ -0,0 +1,6 @@
+/* Question 1: a parameter is a placeholder variiable defined when a function is created. This allows a user or another developer to pass arguments to the function when they call the function from a different piece of code. Depending on the function's creator, the parameters defined maybe mandatory or optional. */
+
+/* Question 2: console.log sends the completed function, or part of it, to a browser's - or editor's - console window. return, on the other hand, sends the results of (or returns to) the function to the piece of code that's calling it */
+
+/* Question 3: letting a function return a value lets javascript, and other languages, influence the behavior of web pages and applications. This can make them dynamic */
+
